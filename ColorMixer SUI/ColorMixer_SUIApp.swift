df488ac5682bb4640c455f5051df88daf78ebce7
Alexander Makarov <fsbisan@mail.ru>
2021-11-01 17:01:24 +0500

    Initial Commit

---
+++ ColorMixer SUI/ColorMixer_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixer_SUIApp.swift
+//  ColorMixer SUI
+//
+//  Created by Александр Макаров on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixer_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
